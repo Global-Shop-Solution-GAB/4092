0cd67334d90b9989fa1f71da6454048f8fef6636
Service Web <sw@gssmail.com>
2024-03-11 12:35:58 -0500

    Commit changes in C:\Websites\Git\emarlow\4092\GCG_4092_Comm_ByPart.sql

---
+++ GCG_4092_Comm_ByPart.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "GCG_4092_Comm_ByPart"(
+ "Trmnl" VARCHAR(50),
+ "Invoice_Number" VARCHAR(6),
+ "Invoice_Date" DATE,
+ "Inv_Tot" NUMERIC(11,2),
+ "Order_Number" VARCHAR(7),
+ "Sales_Tax" NUMERIC(9,2),
+ "Freight" NUMERIC(9,2),
+ "Other_NC" NUMERIC(11,2),
+ "Sales" NUMERIC(11,2),
+ "Part" VARCHAR(20),
+ "Part_NoX" VARCHAR(1),
+ "Comm_Rate" NUMERIC(7,3),
+ "Comm_RateX" VARCHAR(1),
+ "Comm_Dollars" NUMERIC(11,2),
+ "Salesman" VARCHAR(50),
+ "Salsm_Name" VARCHAR(30),
+ "From_Date" DATE,
+ "Thru_Date" DATE,
+ "Cust_Num" VARCHAR(8),
+ "Name" VARCHAR(30),
+ "State" VARCHAR(2)
+);
+
